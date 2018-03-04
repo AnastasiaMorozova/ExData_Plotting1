c794befb52c562880eddd39f5f048ec1d62953b7
AnastasiaMorozova <32030107+AnastasiaMorozova@users.noreply.github.com>
2018-03-04 16:01:34 +0100

    Update graghics.R

--- graghics.R
+++ graghics.R
@@ -2,4 +2,7 @@
 setwd("C:/Users/frost/Documents/DataScience/hpc")
 getwd() # CHECK UP
 
-data<-read.table("household_power_consumption.txt")
+# read tha data
+data<-read.table("household_power_consumption.txt",header = TRUE,sep = ";",na.strings = "?")
+data<-as.data.frame(data)
+
