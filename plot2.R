d256743f61bc9d862ffe7f2fecb837bb432d6645
AnastasiaMorozova <32030107+AnastasiaMorozova@users.noreply.github.com>
2018-03-05 15:42:33 +0100

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -21,15 +21,19 @@ data$days<-weekdays(data$Date,abbreviate=TRUE)
 
 sample<-filter(data,data$Date>=as.Date("2007-02-01")&data$Date<=as.Date("2007-02-02"))
 
+
 # Plot 2
 
 ## call a function hist() to create a histogram with y-axis label and no x-axis labels
 plot(sample$Global_active_power, ylab = "Global Active Power (kilowatts)",xlab="",type = "l",xaxt = 'n')
+
 # annoteting the plot by adding the days of the week on the x-axis of the plot
 step<-sum(sample$days == "Fri")-1
 lengthframe<-nrow(sample)
 axis(1,at=seq(1, lengthframe, by=step),labels=list("Thu","Fri","Sat"))
+
 ## copy the histogram to a png file
 dev.copy(png,file="plot2.png",width=480,height=480)
+
 ## close the png device
 dev.off()
